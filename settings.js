0c12ce416b7ac82f51816137aa3faf362aa39975
UNNI MODZ <98470879+41426@users.noreply.github.com>
2022-06-12 21:04:53 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,20 +19,20 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
-global.footer = '©NexusNwInc.'
+global.owner = ['9400787742']
+global.premium = ['9400787742']
+global.ownernomer = '9400787742'
+global.ownername = 'Unni modz'
+global.botname = 'Unni-modz'
+global.footer = 'Unni modz'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
 global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
-global.author = '🐦Star Boy Nexus'
+global.packname = 'Unni'
+global.author = 'Yt'
 global.sessionName = 'session'
-global.prefa = ['','!','.','🐦','🐤','🗿']
+global.prefa = ['','!','.','💦','🌚','🗿']
 global.sp = '⭔'
 global.mess = {
     success: 'Done ✓',
